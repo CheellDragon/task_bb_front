d2e3b301cb24ac723284c50c1abbc0f57f9a5871
Нұржан Өскенбаев <nurzhan98n@gmail.com>
2023-04-09 12:51:38 +0600

    Сделал спиннер по больше чтобы было видно

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -4,8 +4,8 @@ import Box from '@mui/material/Box';
 
 export default function Spinner() {
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CircularProgress />
+    <Box sx={{ display: 'flex', left: "calc(50% - 15vh)",top:"30vh", justifyContent: "center", position: 'absolute'}}>
+      <CircularProgress size="30vh"/>
     </Box>
   );
 }
